f4f277faa00c370c82eddc7315f5c5f877148698
Milo Davis <davis.mil@husky.neu.edu>
2017-02-07 18:49:20 -0500

    Added single test of universe

---
+++ test/com/nutrons/steamworks/TestUniverse.java
@@ -0,0 +1,12 @@
+package com.nutrons.steamworks;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestUniverse {
+  @Test
+  public void testUniverse() {
+    // Is the universe still functioning?
+    assertEquals(0, 0);
+  }
+}
