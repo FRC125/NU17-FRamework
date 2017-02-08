04c852d4c18ed8753ab4986dd640c11944fab29a
Man Nguyen <sytect@hotmail.com>
2017-02-07 20:36:40 -0500

    Reformatted to google-style

--- src/com/nutrons/steamworks/RobotBootstrapper.java
+++ src/com/nutrons/steamworks/RobotBootstrapper.java
@@ -6,30 +6,31 @@ import com.nutrons.framework.controllers.Talon;
 import com.nutrons.framework.inputs.WpiXboxGamepad;
 
 public class RobotBootstrapper extends Robot {
-    private Talon intakeController;
-    private WpiXboxGamepad controller;
-    private Talon shooterMotor1;
-    private Talon shooterMotor2;
-    private Talon topHopperMotor;
-    private Talon spinHopperMotor;
 
-    @Override
-    protected void constructStreams() {
-        this.topHopperMotor = new Talon(RobotMap.TOP_HOPPER_MOTOR);
-        this.spinHopperMotor = new Talon(RobotMap.SPIN_HOPPER_MOTOR, this.topHopperMotor);
-        this.intakeController = new Talon(RobotMap.INTAKE_MOTOR);
-        this.shooterMotor1 = new Talon(RobotMap.SHOOTER_MOTOR_1);
-        this.shooterMotor2 = new Talon(RobotMap.SHOOTER_MOTOR_2, this.shooterMotor1);
-        this.controller = new WpiXboxGamepad(0);
-        this.enabledStream();
-    }
+  private Talon intakeController;
+  private WpiXboxGamepad controller;
+  private Talon shooterMotor1;
+  private Talon shooterMotor2;
+  private Talon topHopperMotor;
+  private Talon spinHopperMotor;
 
-    @Override
-    protected StreamManager provideStreamManager() {
-        StreamManager sm = new StreamManager(this);
-        sm.registerSubsystem(new Shooter(shooterMotor1));
-        sm.registerSubsystem(new Feeder(intakeController));
-        sm.registerSubsystem(new Hopper(spinHopperMotor));
-        return sm;
-    }
+  @Override
+  protected void constructStreams() {
+    this.topHopperMotor = new Talon(RobotMap.TOP_HOPPER_MOTOR);
+    this.spinHopperMotor = new Talon(RobotMap.SPIN_HOPPER_MOTOR, this.topHopperMotor);
+    this.intakeController = new Talon(RobotMap.INTAKE_MOTOR);
+    this.shooterMotor1 = new Talon(RobotMap.SHOOTER_MOTOR_1);
+    this.shooterMotor2 = new Talon(RobotMap.SHOOTER_MOTOR_2, this.shooterMotor1);
+    this.controller = new WpiXboxGamepad(0);
+    this.enabledStream();
+  }
+
+  @Override
+  protected StreamManager provideStreamManager() {
+    StreamManager sm = new StreamManager(this);
+    sm.registerSubsystem(new Shooter(shooterMotor1));
+    sm.registerSubsystem(new Feeder(intakeController));
+    sm.registerSubsystem(new Hopper(spinHopperMotor));
+    return sm;
+  }
 }
