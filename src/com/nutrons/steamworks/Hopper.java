500bdc684f0008e162661642b489793e6fd5a69a
Man Nguyen <sytect@hotmail.com>
2017-02-07 20:50:44 -0500

    Fixed Hopper modifier order

--- src/com/nutrons/steamworks/Hopper.java
+++ src/com/nutrons/steamworks/Hopper.java
@@ -6,23 +6,21 @@ import com.nutrons.framework.controllers.Events;
 import io.reactivex.Flowable;
 import io.reactivex.functions.Consumer;
 
-
 public class Hopper implements Subsystem {
 
   // TODO: tune as needed
-  private final static double SPIN_POWER = 0.9;
+  private static final double SPIN_POWER = 0.9;
   private final Flowable<ControllerEvent> runHopper;
   private final Consumer<ControllerEvent> hopperController;
 
   /**
-   * @param hopperController passes in RunAtPowerEvent
+   * @param hopperController passes in RunAtPowerEvent.
    */
   public Hopper(Consumer<ControllerEvent> hopperController) {
     this.runHopper = Flowable.just(Events.power(SPIN_POWER));
     this.hopperController = hopperController;
 
   }
-
 
   @Override
   public void registerSubscriptions() {
