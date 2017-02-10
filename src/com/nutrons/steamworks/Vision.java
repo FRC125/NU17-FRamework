7c8fbbc0c3f6b06ffc074b5ff0c63780b7a37577
cappuccinocat <xinglydia@gmail.com>
2017-02-10 12:07:09 -0500

    added dummy variable to fix exception of 'no serial port found'

--- src/com/nutrons/steamworks/Vision.java
+++ src/com/nutrons/steamworks/Vision.java
@@ -8,12 +8,15 @@ public class Vision {
   private Flowable<Double[]> dataStreamDouble;
   private Flowable<Double> angle;
   private Flowable<Double> distance;
+  private static final String DUMMY_VALUE = "-1000.0:-1000.0"; //Arduino sends -1000.0 over serial when it doesn't see anything, to prevent
+                                                               //robot sending an exception "no serial port found"
 
   Vision(Flowable<byte[]> dataStream) {
     this.dataStream = dataStream;
     this.dataStreamDouble = this.dataStream
         .filter(x -> x.length == 10)
         .map(x -> new String(x, "UTF-8"))
+        .map(x -> x == DUMMY_VALUE ? "0.0:0.0" : x) //vision will change any dummy values to 0.0
         .map(x -> x.split(":")).filter(x -> x.length == 2)
         .map(x -> new Double[]{Double.valueOf(x[0]),
             Double.valueOf(x[1])}); //Returns a double array[distance, angle]
