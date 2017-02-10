d1fd6d5fea3c557de1101089cd30e49332e87e89
Asher Gottlieb <totally.ashes@gmail.com>
2017-02-10 18:18:02 -0500

    updated framework, simplified implementations

--- src/com/nutrons/steamworks/Hopper.java
+++ src/com/nutrons/steamworks/Hopper.java
@@ -1,28 +1,20 @@
 package com.nutrons.steamworks;
 
 import com.nutrons.framework.Subsystem;
-import com.nutrons.framework.controllers.ControllerEvent;
-import com.nutrons.framework.controllers.Events;
-import io.reactivex.Flowable;
-import io.reactivex.functions.Consumer;
+import com.nutrons.framework.controllers.LoopSpeedController;
 
 public class Hopper implements Subsystem {
 
   // TODO: tune as needed
   private static final double SPIN_POWER = 0.9;
-  private final Flowable<ControllerEvent> runHopper;
-  private final Consumer<ControllerEvent> hopperController;
-
-  /**
-   * @param hopperController passes in RunAtPowerEvent.
-   */
-  public Hopper(Consumer<ControllerEvent> hopperController) {
-    this.runHopper = Flowable.just(Events.power(SPIN_POWER));
+  private final LoopSpeedController hopperController;
+  
+  public Hopper(LoopSpeedController hopperController) {
     this.hopperController = hopperController;
   }
 
   @Override
   public void registerSubscriptions() {
-    runHopper.subscribe(hopperController);
+    hopperController.runAtPower(SPIN_POWER);
   }
 }
