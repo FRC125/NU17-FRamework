1466d7393f3a5f64e925a743b6c571b0881fcac0
Brian Estevez <goodguybry.be@gmail.com>
2017-02-13 14:55:10 -0500

    Fixed naming conventions

--- src/com/nutrons/steamworks/Climbtake.java
+++ src/com/nutrons/steamworks/Climbtake.java
@@ -6,28 +6,28 @@ import com.nutrons.framework.controllers.LoopSpeedController;
 import io.reactivex.Flowable;
 
 public class Climbtake implements Subsystem {
-  private static final double INTAKE_SPEED = -1.0;
-  private static final double CLIMBER_SPEED = 1.0;
-  private final LoopSpeedController intakeController;
-  private final LoopSpeedController climberController;
+  private static final double CLIMBTAKE_SPEED_LEFT = 1.0;
+  private static final double CLIMBTAKE_SPEED_RIGHT = 1.0;
+  private final LoopSpeedController climbtakeControllerLeft;
+  private final LoopSpeedController climbtakeControllerRight;
   private final Flowable<Boolean> forward;
   private final Flowable<Boolean> reverse;
 
-  public Climbtake(LoopSpeedController intakeController, LoopSpeedController climberController,
+  public Climbtake(LoopSpeedController climbtakeControllerLeft, LoopSpeedController climbtakeControllerRight,
                    Flowable<Boolean> forward, Flowable<Boolean> reverse) {
-    this.intakeController = intakeController;
-    this.climberController = climberController;
+    this.climbtakeControllerLeft = climbtakeControllerLeft;
+    this.climbtakeControllerRight = climbtakeControllerRight;
     this.forward = forward;
     this.reverse = reverse;
   }
 
   @Override
   public void registerSubscriptions() {
-    forward.map(b -> b ? INTAKE_SPEED : 0.0).map(Events::power).subscribe(intakeController);
-    forward.map(b -> b ? CLIMBER_SPEED : 0.0).map(Events::power).subscribe(climberController);
+    forward.map(b -> b ? CLIMBTAKE_SPEED_LEFT : 0.0).map(Events::power).subscribe(climbtakeControllerLeft);
+    forward.map(b -> b ? CLIMBTAKE_SPEED_RIGHT : 0.0).map(Events::power).subscribe(climbtakeControllerRight);
 
-    reverse.map(b -> b ? -INTAKE_SPEED : 0.0).map(Events::power).subscribe(intakeController);
-    reverse.map(b -> b ? -CLIMBER_SPEED : 0.0).map(Events::power).subscribe(climberController);
+    reverse.map(b -> b ? -CLIMBTAKE_SPEED_LEFT : 0.0).map(Events::power).subscribe(climbtakeControllerLeft);
+    reverse.map(b -> b ? -CLIMBTAKE_SPEED_RIGHT : 0.0).map(Events::power).subscribe(climbtakeControllerRight);
   }
 }
 
