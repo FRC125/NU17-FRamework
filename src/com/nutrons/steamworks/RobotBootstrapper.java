724bb0f0b086830b899898deeb296b07acfa2118
Asher Gottlieb <totally.ashes@gmail.com>
2017-02-16 13:02:10 -0500

    reverted changes to some files, uncommented lines

--- src/com/nutrons/steamworks/RobotBootstrapper.java
+++ src/com/nutrons/steamworks/RobotBootstrapper.java
@@ -1,8 +1,10 @@
 package com.nutrons.steamworks;
 
+import com.ctre.CANTalon;
 import com.nutrons.framework.Robot;
 import com.nutrons.framework.StreamManager;
 import com.nutrons.framework.controllers.ControlMode;
+import com.nutrons.framework.controllers.Events;
 import com.nutrons.framework.controllers.Talon;
 import com.nutrons.framework.inputs.CommonController;
 import com.nutrons.framework.inputs.HeadingGyro;
@@ -33,10 +35,9 @@ public class RobotBootstrapper extends Robot {
   @Override
   protected void constructStreams() {
 
-    //this.serial = new Serial(PACKET_LENGTH *2, PACKET_LENGTH);
-    //this.vision = Vision.getInstance(serial.getDataStream());
+    this.serial = new Serial(PACKET_LENGTH *2, PACKET_LENGTH);
+    this.vision = Vision.getInstance(serial.getDataStream());
 
-     /*
     this.hoodMaster = new Talon(RobotMap.HOOD_MOTOR_A,
             CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
             Events.setOutputVoltage(-12f, +12f).actOn(this.hoodMaster);
@@ -48,7 +49,7 @@ public class RobotBootstrapper extends Robot {
     this.intakeController = new Talon(RobotMap.INTAKE_MOTOR);
     this.shooterMotor1 = new Talon(RobotMap.SHOOTER_MOTOR_1);
     this.shooterMotor2 = new Talon(RobotMap.SHOOTER_MOTOR_2, this.shooterMotor1);
-    */
+
     // Drivetrain Motors
     this.leftLeader = new Talon(RobotMap.FRONT_LEFT);
     this.leftLeader.setControlMode(ControlMode.MANUAL);
@@ -73,15 +74,14 @@ public class RobotBootstrapper extends Robot {
     StreamManager sm = new StreamManager(this);
     sm.registerSubsystem(this.driverPad);
     sm.registerSubsystem(this.operatorPad);
-    /*
+
     sm.registerSubsystem(new Turret(vision.getAngle(), hoodMaster));
     sm.registerSubsystem(new Shooter(shooterMotor1));
     sm.registerSubsystem(new Feeder(intakeController));
     sm.registerSubsystem(new Hopper(spinHopperMotor));
-    sm.registerSubsystem(new ButtonTest(driverPad.button(1)));
-    Events.mode(ControlMode.MANUAL).actOn(leftLeader);
-    Events.mode(ControlMode.MANUAL).actOn(rightLeader);
-    */
+
+    leftLeader.setControlMode(ControlMode.MANUAL);
+    rightLeader.setControlMode(ControlMode.MANUAL);
     sm.registerSubsystem(new Drivetrain(driverPad.button(1),
         gyro.getGyroReadings(), Flowable.just(0.0),
         driverPad.rightStickX().map(x -> -x), driverPad.leftStickY().map(x -> x),
--- src/com/nutrons/steamworks/RobotMap.java
+++ src/com/nutrons/steamworks/RobotMap.java
@@ -2,7 +2,7 @@ package com.nutrons.steamworks;
 
 public class RobotMap {
   // Intake or Shooter
-  /*
+
   public static final int INTAKE_MOTOR = 4;
   public static final int SHOOTER_MOTOR_1 = 2;
   public static final int SHOOTER_MOTOR_2 = 3;
@@ -10,7 +10,7 @@ public class RobotMap {
   public static final int TOP_HOPPER_MOTOR = 0;
   public static final int SPIN_HOPPER_MOTOR = 0;
   public static final int HOOD_MOTOR_A = 3;
-  */
+
   // Ports of wheels TODO: Fix ports to match robot motors
   public static final int FRONT_LEFT = 1;
   public static final int BACK_LEFT = 20;
