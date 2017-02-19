502898b4b1a2e541d4ba8f7c96f139f6b603d7dc
nutronsds <nutronsds@gmail.com>
2017-02-19 18:48:01 -0500

    reverted RobotMap

--- src/com/nutrons/steamworks/RobotMap.java
+++ src/com/nutrons/steamworks/RobotMap.java
@@ -3,21 +3,21 @@ package com.nutrons.steamworks;
 public class RobotMap {
 
   // Intake or Shooter
-  public static final int SHOOTER_MOTOR_1 = 22;
+  public static final int SHOOTER_MOTOR_1 = 2;
   public static final int SHOOTER_MOTOR_2 = 3;
   //TODO: Change climber ports to match robot.
   public static final int CLIMBTAKE_MOTOR_1 = 12;
   public static final int CLIMBTAKE_MOTOR_2 = 13;
   // TODO: Change hopper ports to match robot
   public static final int TOP_HOPPER_MOTOR = 6;
-  public static final int SPIN_FEEDER_MOTOR = 25;
-  public static final int HOOD_MOTOR_A = 23;
+  public static final int SPIN_FEEDER_MOTOR = 4;
+  public static final int HOOD_MOTOR_A = 5;
 
   // Ports of Drivetrain
-  public static final int FRONT_LEFT = 5;
-  public static final int BACK_LEFT = 4;
-  public static final int FRONT_RIGHT = 2;
-  public static final int BACK_RIGHT = 1;
+  public static final int FRONT_LEFT = 1;
+  public static final int BACK_LEFT = 20;
+  public static final int FRONT_RIGHT = 14;
+  public static final int BACK_RIGHT = 15;
 
   // Controllers
   public static final int OP_PAD = 0;
