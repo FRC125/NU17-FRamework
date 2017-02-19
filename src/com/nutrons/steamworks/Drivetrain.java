a3960f1556f4b3314cef7a393f5d0a88e6c1540b
Josh Young <SourceEpicSquidx@gmail.com>
2017-02-19 13:27:28 -0500

    Fixed style

--- src/com/nutrons/steamworks/Drivetrain.java
+++ src/com/nutrons/steamworks/Drivetrain.java
@@ -1,10 +1,10 @@
 package com.nutrons.steamworks;
 
 import static com.nutrons.framework.util.FlowOperators.combineDisposable;
-import static com.nutrons.framework.util.FlowOperators.toFlow;
 import static com.nutrons.framework.util.FlowOperators.deadbandMap;
 import static com.nutrons.framework.util.FlowOperators.limitWithin;
 import static com.nutrons.framework.util.FlowOperators.pidLoop;
+import static com.nutrons.framework.util.FlowOperators.toFlow;
 import static io.reactivex.Flowable.combineLatest;
 
 import com.nutrons.framework.Subsystem;
@@ -32,6 +32,7 @@ public class Drivetrain implements Subsystem {
   private final Flowable<Double> yaw;
   private final Consumer<ControllerEvent> leftDrive;
   private final Consumer<ControllerEvent> rightDrive;
+
   private Flowable<Double> targetHeading;
   private final Flowable<Double> error;
   private final Flowable<Double> output;
@@ -41,6 +42,7 @@ public class Drivetrain implements Subsystem {
   private static final double ANGLE_I = 0.0;
   private static final double ANGLE_D = 0.0065;
   private static final int ANGLE_BUFFER_LENGTH = 10;
+
   private final Flowable<Boolean> slowDrive;
   private final double slowDriveCoeff = 0.33;
   private double flip;
@@ -55,7 +57,7 @@ public class Drivetrain implements Subsystem {
    * @param rightDrive all controllers on the right of the drivetrain
    */
 
-  public Drivetrain( Talon leftLeader,
+  public Drivetrain(Talon leftLeader,
       Talon rightLeader,
       HeadingGyro gyro,
       Flowable<Boolean> holdHeading,
@@ -75,7 +77,7 @@ public class Drivetrain implements Subsystem {
     this.leftDrive = leftDrive;
     this.rightDrive = rightDrive;
     this.targetHeading = targetHeading;
-    this.error = combineLatest(targetHeading, currentHeading, (x, y) -> x - y);
+    this.error = combineLatest(this.targetHeading, currentHeading, (x, y) -> x - y);
     this.output = error
         .compose(pidLoop(ANGLE_P, ANGLE_BUFFER_LENGTH, ANGLE_I, ANGLE_D));
     this.holdHeading = holdHeading;
