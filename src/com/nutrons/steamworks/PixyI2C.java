6e8fce037e6cbc55da0abdb1c7eb4e64fd7e549b
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-19 21:48:42 -0500

    Added i2c class w-Brinbri

--- src/com/nutrons/steamworks/PixyI2C.java
+++ src/com/nutrons/steamworks/PixyI2C.java
@@ -1,108 +1,78 @@
-class PixyI2C {
-#include <SPI.h>
-#include <Pixy.h>
+package com.nutrons.steamworks;
 
-Pixy pixy;
-uint16_t blocks;
-String toSend;
-String stateToSend; 
+import static com.nutrons.steamworks.PixyI2C.State.NONE;
+import static java.lang.Math.abs;
+import static java.lang.Math.tan;
 
-const double FOV_X = 75; //horizontal Field of View in degrees
-const double FOV_Y = 47; //vertical Field of View in degrees
-const double RESOLUTION_WIDTH = 320; //in pixels, 320 x 200
-const double RESOLUTION_HEIGHT = 200; //in pixels
-const double CAMERA_ANGLE = 0.0; //in degrees -- see wiki
-const double CAMERA_HEIGHT = 0.0; //in inches
+public class PixyI2C {
+  private static final double FOV_X = 75; //horizontal Field of View in degrees
+  private static final double FOV_Y = 47; //vertical Field of View in degrees
+  private static final double RESOLUTION_WIDTH = 320; //in pixels, 320 x 200
+  private static final double RESOLUTION_HEIGHT = 200; //in pixels
+  private static final double CAMERA_ANGLE = 0.0; //in degrees -- see wiki
+  private static final double CAMERA_HEIGHT = 0.0; //in inches
+  //TODO: Edit these values
+  private static final double VERTICAL_ZERO_OFFSET_BOILER = 0.0; //in degrees -- see wiki
+  private static final double TARGET_HEIGHT_BOILER = 0.0; //in inches
+  private static final double VERTICAL_ZERO_OFFSET_GEAR = 0.0; //in degrees -- see wiki
+  private static final double TARGET_HEIGHT_GEAR = 0.0; //in inches
+  int blocks;
+  String toSend;
+  String stateToSend;
+  double angleToTarget_x; //horizontal angle in degrees
+  double angleToTarget_y; //vertical angle in degrees
+  double distanceToTarget; //inches
+  //default values
+  State currentState = NONE;
 
-//TODO: Edit these values
-const double VERTICAL_ZERO_OFFSET_BOILER = 0.0; //in degrees -- see wiki
-const double TARGET_HEIGHT_BOILER = 0.0; //in inches
+  ;
 
-const double VERTICAL_ZERO_OFFSET_GEAR = 0.0; //in degrees -- see wiki
-const double TARGET_HEIGHT_GEAR = 0.0; //in inches
+  public void visionLogic(Block[] blocks) {
+    //make sure we see two blocks
+    if (blocks.length == 2) {
+      //checks that vertical distance between blocks is greater
+      // than horizontal distance between blocks, meaning that we are seeing the boiler target
+      if (abs(blocks[0].y - blocks[1].y) > abs(blocks[0].x - blocks[1].x)) {
+        angleToTarget_x = getHorizontalAngleOffset(blocks[0].x);
+        angleToTarget_y = getVerticalAngleOffset(blocks[0].y, VERTICAL_ZERO_OFFSET_BOILER);
+        distanceToTarget = getDistance(angleToTarget_y, TARGET_HEIGHT_BOILER);
 
-double angleToTarget_x; //horizontal angle in degrees
-double angleToTarget_y; //vertical angle in degrees
-double distanceToTarget; //inches
-
-enum state {
-  NONE,
-  BOIL,
-  GEAR
-};
-
-//default values
-state currentState = NONE;
-
-void setup() {
-  Serial.begin(9600);
-  pixy.init();
-}
-
-void loop() { 
-  blocks = pixy.getBlocks(2);
-  
-  //make sure we see two blocks
-  if (blocks == 2) { 
-     if(abs(pixy.blocks[0].y - pixy.blocks[1].y) > abs(pixy.blocks[0].x - pixy.blocks[1].x)){ //checks that vertical distance between blocks is greater than horizontal distance between blocks, meaning that we are seeing the boiler target
-       angleToTarget_x = getHorizontalAngleOffset(pixy.blocks[0].x);
-       angleToTarget_y = getVerticalAngleOffset(pixy.blocks[0].y, VERTICAL_ZERO_OFFSET_BOILER);
-       distanceToTarget = getDistance(angleToTarget_y, TARGET_HEIGHT_BOILER);
-
-       currentState = BOIL;
-       writeBytes(distanceToTarget, angleToTarget_x);
-     }else{ //then that means we're seeing the gears!
-       angleToTarget_x = getHorizontalAngleOffset(pixy.blocks[0].x);
-       angleToTarget_y = getVerticalAngleOffset(pixy.blocks[0].y, VERTICAL_ZERO_OFFSET_GEAR);
-       distanceToTarget = getDistance(angleToTarget_y, TARGET_HEIGHT_GEAR);
-       
-       currentState = GEAR;
-       writeBytes(distanceToTarget, angleToTarget_x);
-     }
+      }
+    }
   }
 
-  //If we don't see anything, just send -1000 for both values!
-  currentState = NONE;
-  writeBytes(-1000.0, -1000.0);
-}
+  public double getHorizontalAngleOffset(double x) {
+    return (x * FOV_X / RESOLUTION_WIDTH) - 37.5;
+  }
+
+  public double getVerticalAngleOffset(double y, double verticalZeroOffset) {
+    return (verticalZeroOffset - (y * FOV_Y / RESOLUTION_HEIGHT)) + CAMERA_ANGLE;
+  }
 
 //******THE IMPORTANT STUFF******
 
-double getHorizontalAngleOffset(double x){
-  return (x*FOV_X/RESOLUTION_WIDTH) - 37.5;
-}
-
-double getVerticalAngleOffset(double y, double verticalZeroOffset) {
-  return (verticalZeroOffset - (y*FOV_Y/RESOLUTION_HEIGHT )) + CAMERA_ANGLE; 
-}
-
-double degreesToRadians(double deg){
-  return (deg * 3.1415926)/180;
-}
-
-double getDistance(double angleToTargetY, double targetHeight){
-  return (targetHeight - CAMERA_HEIGHT)/tan(degreesToRadians((angleToTargetY)));
-}
-
-void writeBytes(double distance, double angle){
-  switch(currentState) {
-    case NONE:
-      stateToSend = "NONE";
-      break;
-    case GEAR:
-      stateToSend = "GEAR";
-      break;
-    case BOIL:
-      stateToSend = "BOIL";
-      break;
-    default:
-      Serial.print("Easter egg! You have been visited by the cat of good wishes! =(^-^)=");
-      break;
+  public double degreesToRadians(double deg) {
+    return (deg * 3.1415926) / 180;
   }
-  toSend = stateToSend + ":" + String(distance, 4).substring(0,5) + ":" + String(angle, 4).substring(0,5) + "\n";
-  byte sendBytes[toSend.length() + 1];
-  toSend.getBytes(sendBytes, toSend.length() + 1);
-  Serial.write(sendBytes, toSend.length() + 1);
-  Serial.flush();
-}
+
+  public double getDistance(double angleToTargetY, double targetHeight) {
+    return (targetHeight - CAMERA_HEIGHT) / tan(degreesToRadians((angleToTargetY)));
+  // You have been Visited From the cat god =(-|-)=
+  }
+
+  enum State {
+    NONE,
+    BOIL,
+    GEAR
+  }
+
+  class Block {
+    private final double x;
+    private final double y;
+
+    Block(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
 }
