9a06e1e508b4076b87ea4a930831780f0de2388d
Brian Estevez <goodguybry.be@gmail.com>
2017-02-20 11:10:31 -0500

    Fixed style for i2c, fixed drivetrain issue with .map

--- src/com/nutrons/steamworks/PixyI2C.java
+++ src/com/nutrons/steamworks/PixyI2C.java
@@ -5,6 +5,7 @@ import static java.lang.Math.abs;
 import static java.lang.Math.tan;
 
 public class PixyI2C {
+
   private static final double FOV_X = 75; //horizontal Field of View in degrees
   private static final double FOV_Y = 47; //vertical Field of View in degrees
   private static final double RESOLUTION_WIDTH = 320; //in pixels, 320 x 200
@@ -19,37 +20,37 @@ public class PixyI2C {
   int blocks;
   String toSend;
   String stateToSend;
-  double angleToTarget_x; //horizontal angle in degrees
-  double angleToTarget_y; //vertical angle in degrees
+  double angleToTargetX; //horizontal angle in degrees
+  double angleToTargetY; //vertical angle in degrees
   double distanceToTarget; //inches
   //default values
   State currentState = NONE;
 
-  ;
-
+  /** Makes sure we see two blocks.
+   * Then takes the horizontal distance between blocks,
+   * meaning that we are seeing the boiler target
+   * @param blocks Array of blocks
+   */
   public void visionLogic(Block[] blocks) {
-    //make sure we see two blocks
     if (blocks.length == 2) {
-      //checks that vertical distance between blocks is greater
-      // than horizontal distance between blocks, meaning that we are seeing the boiler target
-      if (abs(blocks[0].y - blocks[1].y) > abs(blocks[0].x - blocks[1].x)) {
-        angleToTarget_x = getHorizontalAngleOffset(blocks[0].x);
-        angleToTarget_y = getVerticalAngleOffset(blocks[0].y, VERTICAL_ZERO_OFFSET_BOILER);
-        distanceToTarget = getDistance(angleToTarget_y, TARGET_HEIGHT_BOILER);
+      if (abs(blocks[0].yval - blocks[1].yval) > abs(blocks[0].xval - blocks[1].xval)) {
+        angleToTargetX = getHorizontalAngleOffset(blocks[0].xval);
+        angleToTargetY = getVerticalAngleOffset(blocks[0].yval, VERTICAL_ZERO_OFFSET_BOILER);
+        distanceToTarget = getDistance(angleToTargetY, TARGET_HEIGHT_BOILER);
 
       }
     }
   }
 
-  public double getHorizontalAngleOffset(double x) {
-    return (x * FOV_X / RESOLUTION_WIDTH) - 37.5;
+  public double getHorizontalAngleOffset(double angleOffsetX) {
+    return (angleOffsetX * FOV_X / RESOLUTION_WIDTH) - 37.5;
   }
 
-  public double getVerticalAngleOffset(double y, double verticalZeroOffset) {
-    return (verticalZeroOffset - (y * FOV_Y / RESOLUTION_HEIGHT)) + CAMERA_ANGLE;
+  public double getVerticalAngleOffset(double angleOffsetY, double verticalZeroOffset) {
+    return (verticalZeroOffset - (angleOffsetY * FOV_Y / RESOLUTION_HEIGHT)) + CAMERA_ANGLE;
   }
 
-//******THE IMPORTANT STUFF******
+  //******THE IMPORTANT STUFF******
 
   public double degreesToRadians(double deg) {
     return (deg * 3.1415926) / 180;
@@ -57,7 +58,7 @@ public class PixyI2C {
 
   public double getDistance(double angleToTargetY, double targetHeight) {
     return (targetHeight - CAMERA_HEIGHT) / tan(degreesToRadians((angleToTargetY)));
-  // You have been Visited From the cat god =(-|-)=
+    // You have been Visited From the cat god =(-|-)=
   }
 
   enum State {
@@ -67,12 +68,13 @@ public class PixyI2C {
   }
 
   class Block {
-    private final double x;
-    private final double y;
 
-    Block(double x, double y) {
-      this.x = x;
-      this.y = y;
+    private final double xval;
+    private final double yval;
+
+    Block(double blockX, double blockY) {
+      this.xval = blockX;
+      this.yval = blockY;
     }
   }
 }
