ecf07d86830b6dca7cf228ec9504780fcfb188a2
cappuccinocat <xinglydia@gmail.com>
2017-02-21 16:06:54 -0500

    added distance function, but need to edit constants

--- src/com/nutrons/steamworks/VisionProcessor.java
+++ src/com/nutrons/steamworks/VisionProcessor.java
@@ -1,5 +1,6 @@
 package com.nutrons.steamworks;
 
+import com.nutrons.libKudos254.Rotation2d;
 import com.nutrons.libKudos254.vision.TargetInfo;
 import com.nutrons.libKudos254.vision.VisionUpdate;
 import com.nutrons.libKudos254.vision.VisionUpdateReceiver;
@@ -18,9 +19,17 @@ import static com.nutrons.framework.util.FlowOperators.toFlow;
  * @see
  */
 public class VisionProcessor implements VisionUpdateReceiver {
+  // Pose of the camera frame w.r.t. the turret frame
+  public static double kCameraZOffset = 19.75;
+  public static double kCameraPitchAngleDegrees = 35.75; // calibrated 4/22
+  public static double kCameraYawAngleDegrees = -1.0;
+
   static VisionProcessor instance_ = new VisionProcessor();
   VisionUpdate update_ = null;
-  public static final double CAMERA_HEIGHT = 10.0; //in inches
+  public static final double CENTER_OF_TARGET_HEIGHT = 89.0;
+  double differential_height_ = CENTER_OF_TARGET_HEIGHT - kCameraZOffset;
+  Rotation2d camera_pitch_correction_ = Rotation2d.fromDegrees(kCameraPitchAngleDegrees);;
+  Rotation2d camera_yaw_correction_ = Rotation2d.fromDegrees(kCameraYawAngleDegrees);
 
   VisionProcessor() {
   }
@@ -50,12 +59,30 @@ public class VisionProcessor implements VisionUpdateReceiver {
     return 0.0;
   }
 
-  double getDistance(double angleToTargetY, double targetHeight){
-    return (targetHeight - CAMERA_HEIGHT)/Math.tan(Math.toRadians((angleToTargetY)));
+  double getDistance(){
+    if (!(update_.getTargets() == null || update_.getTargets().isEmpty())) {
+      for (TargetInfo target : update_.getTargets()) {
+        double xyaw = target.getX() * camera_yaw_correction_.cos() + camera_yaw_correction_.sin();
+        double yyaw = camera_yaw_correction_.cos() - target.getX() * camera_yaw_correction_.sin();
+        double zyaw = target.getZ();
+
+        // Compensate for camera pitch
+        double xr = zyaw * camera_pitch_correction_.sin() + xyaw * camera_pitch_correction_.cos();
+        double yr = yyaw;
+        double zr = zyaw * camera_pitch_correction_.cos() - xyaw * camera_pitch_correction_.sin();
+
+        // find intersection with the goal
+        if (zr > 0) {
+          double scaling = differential_height_ / zr;
+          double distance = Math.hypot(xr, yr) * scaling;
+          Rotation2d angle = new Rotation2d(xr, yr, true);
+        }
+      }
+    }
+    return 0.0;
   }
 
   public Flowable<Double> getHorizAngleFlow(){
-    //System.out.println(this.getYawHorizAngle());
     return toFlow(() -> this.getYawHorizAngle());
   }
 
