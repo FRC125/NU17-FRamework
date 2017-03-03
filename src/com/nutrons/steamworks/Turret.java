8a19c9005b73881afc0176b04bb0ebfafeba819a
Asher Gottlieb <totally.ashes@gmail.com>
2017-03-03 00:23:48 -0500

    made turret changes, hopefully commandified

--- src/com/nutrons/steamworks/Turret.java
+++ src/com/nutrons/steamworks/Turret.java
@@ -16,6 +16,7 @@ public class Turret implements Subsystem {
   private static final double DVAL = 12.5;
   private static final double FVAL = 0.0;
   private static final double MOTOR_ROTATIONS_TO_TURRET_ROTATIONS = (double) 104 / 22;
+  private static final double TOLERANCE_DEGREES = 1.0;
   private final Flowable<Double> angle;
   private final Flowable<Double> distance;
   private final Talon hoodMaster;
@@ -23,8 +24,8 @@ public class Turret implements Subsystem {
   private final Flowable<Boolean> fwdLim;
   private final Flowable<Double> joyControl; //TODO: Remoove
   private final Flowable<Boolean> aimButton;
+  private final Flowable<Double> setpoint;
   private Flowable<Double> position;
-  private static final double TOLERANCE_DEGREES = 1.0;
 
   /**
    * The Turret System that is used for aiming our shooter.
@@ -37,52 +38,41 @@ public class Turret implements Subsystem {
                 Talon master,
                 Flowable<Double> joyControl,
                 Flowable<Boolean> aimButton) { //TODO: remove joycontrol
-    this.angle = angle.map(x -> Math.toDegrees(x));
+    this.angle = angle.map(Math::toDegrees);
     this.distance = distance;
     this.hoodMaster = master;
     Events.resetPosition(0.0).actOn(this.hoodMaster);
     this.revLim = FlowOperators.toFlow(this.hoodMaster::revLimitSwitchClosed);
     this.fwdLim = FlowOperators.toFlow(this.hoodMaster::fwdLimitSwitchClosed);
     this.joyControl = joyControl;
-    this.position = FlowOperators.toFlow(() -> this.hoodMaster.position());
+    this.position = FlowOperators.toFlow(this.hoodMaster::position);
     this.aimButton = aimButton;
+    this.setpoint = this.angle
+        .map(x -> (x * MOTOR_ROTATIONS_TO_TURRET_ROTATIONS) / 360.0); //used to be negative
   }
 
-  public Command pulse() {
+  public Command automagicMode() {
     return Command.just(x -> {
       this.hoodMaster.setControlMode(ControlMode.LOOP_POSITION);
       this.hoodMaster.setPID(PVAL, IVAL, DVAL, FVAL);
-      return Flowable.just(() -> {
-        this.hoodMaster.setControlMode(ControlMode.MANUAL);
-      });
-    });
+      return Flowable.just(() -> this.hoodMaster.runAtPower(0));
+    }).then(Command.fromSubscription(() -> setpoint.map(Events::setpoint).subscribe(hoodMaster)));
   }
 
   @Override
   public void registerSubscriptions() {
-    Flowable<Double> setpoint = this.angle
-        .map(x -> (x * MOTOR_ROTATIONS_TO_TURRET_ROTATIONS) / 360.0); //used to be negative
 
     this.hoodMaster.setReversedSensor(false); //used to be true
 
-    aimButton.subscribe((Boolean b) -> {
-      if (b) {
-        this.hoodMaster.setControlMode(ControlMode.LOOP_POSITION);
-        this.hoodMaster.setPID(PVAL, IVAL, DVAL, FVAL);
-        setpoint.subscribe(x -> Events.setpoint(x).actOn(hoodMaster));
-      } else {
-        this.hoodMaster.setControlMode(ControlMode.MANUAL);
-        FlowOperators.deadband(joyControl).map(x -> Events.power(x ))
-            .subscribe(hoodMaster); //TODO: remove this joystick
-      }
-    });
+    FlowOperators.deadband(joyControl).map(Events::power)
+        .subscribe(hoodMaster);
 
-    FlowOperators.toFlow(() -> hoodMaster.position())
+    FlowOperators.toFlow(hoodMaster::position)
         .subscribe(new WpiSmartDashboard().getTextFieldDouble("position"));
     this.angle.subscribe(new WpiSmartDashboard().getTextFieldDouble("angle"));
-    this.angle.map(x -> x < TOLERANCE_DEGREES ? true : false).subscribe(new WpiSmartDashboard().getTextFieldBoolean("within tolerance, GO!"));
+    this.angle.map(x -> x < TOLERANCE_DEGREES).subscribe(new WpiSmartDashboard().getTextFieldBoolean("within tolerance, GO!"));
     this.distance.subscribe(new WpiSmartDashboard().getTextFieldDouble("distance"));
-    this.distance.map(x -> x > 108 && x < 168 ? true : false).subscribe(new WpiSmartDashboard().getTextFieldBoolean("within distance range, GO!"));
+    this.distance.map(x -> x > 108 && x < 168).subscribe(new WpiSmartDashboard().getTextFieldBoolean("within distance range, GO!"));
     this.revLim.subscribe(new WpiSmartDashboard().getTextFieldBoolean("revLim"));
     this.fwdLim.subscribe(new WpiSmartDashboard().getTextFieldBoolean("fwdLim"));
     setpoint.subscribe(new WpiSmartDashboard().getTextFieldDouble("setpoint"));
