00c58834356ea22eaa283f71aa16bde95eca2cba
Asher Gottlieb <totally.ashes@gmail.com>
2017-03-15 18:12:48 -0400

    turret changes

--- src/com/nutrons/steamworks/Turret.java
+++ src/com/nutrons/steamworks/Turret.java
@@ -1,5 +1,8 @@
 package com.nutrons.steamworks;
 
+import static com.nutrons.framework.util.FlowOperators.deadbandMap;
+import static io.reactivex.Flowable.combineLatest;
+
 import com.nutrons.framework.Subsystem;
 import com.nutrons.framework.commands.Command;
 import com.nutrons.framework.controllers.ControlMode;
@@ -9,11 +12,13 @@ import com.nutrons.framework.subsystems.WpiSmartDashboard;
 import com.nutrons.framework.util.FlowOperators;
 import io.reactivex.Flowable;
 
+import java.util.concurrent.TimeUnit;
+
 public class Turret implements Subsystem {
 
-  private static final double PVAL = 200.0;
-  private static final double IVAL = 10.0;
-  private static final double DVAL = 12.5;
+  private static final double PVAL = 0.45;
+  private static final double IVAL = 0.0;
+  private static final double DVAL = 0.0;
   private static final double FVAL = 0.0;
   private static final double MOTOR_ROTATIONS_TO_TURRET_ROTATIONS = (double) 104 / 22;
   private static final double TOLERANCE_DEGREES = 1.0;
@@ -25,6 +30,7 @@ public class Turret implements Subsystem {
   private final Flowable<Double> joyControl; //TODO: Remoove
   private final Flowable<Boolean> aimButton;
   private final Flowable<Double> setpoint;
+  private Flowable<Double> joyedSetpoint;
   private Flowable<Double> position;
 
   /**
@@ -48,7 +54,10 @@ public class Turret implements Subsystem {
     this.position = FlowOperators.toFlow(this.hoodMaster::position);
     this.aimButton = aimButton;
     this.setpoint = this.angle
-        .map(x -> (x * MOTOR_ROTATIONS_TO_TURRET_ROTATIONS) / 360.0); //used to be negative
+        .map(x -> (x * MOTOR_ROTATIONS_TO_TURRET_ROTATIONS) / 360.0).map(x -> {
+          hoodMaster.accept(Events.resetPosition(0.0));
+          return x;
+        }); //used to be negative
   }
 
   public Command automagicMode() {
@@ -59,17 +68,37 @@ public class Turret implements Subsystem {
         .addFinalTerminator(() -> hoodMaster.runAtPower(0)));
   }
 
+  /**
+   * TeleControl lets the turret auto aim to any target, but also be controlled by the joystick
+   * (by way of altering the setpoint!)
+   * @return a command that aims the turret
+   */
+  public Command teleControl() {
+    return Command.fromAction(() -> {
+      System.out.println("doing teleControl");
+      this.hoodMaster.setControlMode(ControlMode.LOOP_POSITION);
+      this.hoodMaster.setPID(PVAL, IVAL, DVAL, FVAL);
+    }).then(Command.fromSubscription(() -> joyedSetpoint.map(Events::setpoint).subscribe(hoodMaster))
+        .addFinalTerminator(() -> {
+      System.out.println("final terminator");
+      hoodMaster.runAtPower(0);
+        }));
+  }
+
   @Override
   public void registerSubscriptions() {
     this.hoodMaster.setReversedSensor(false); //used to be true
+    //Change joystick control into setpoints, full range is -4.7 to 4.7
 
-    FlowOperators.deadband(joyControl).map(x -> -0.3 * x).map(Events::power)
-        .subscribe(hoodMaster);
-    this.aimButton.filter(x -> x).map(x -> automagicMode().terminable(aimButton.filter(y -> !y)))
-        .subscribe(x -> x.execute(true));
+    this.joyedSetpoint = combineLatest(joyControl.map(deadbandMap(-0.15, 0.15, 0.0)).map(x -> -1.125 * x), this.setpoint
+        //.withLatestFrom(aimButton, (x, y) -> y ? x : 0.0)
+        , (j, s) -> j + s);
 
-    FlowOperators.toFlow(hoodMaster::position)
-        .subscribe(new WpiSmartDashboard().getTextFieldDouble("position"));
+    //this.joyedSetpoint.subscribe(System.out::println);
+
+    /**FlowOperators.deadfband(joyControl).map(x -> -0.3 * x).map(Events::power).share()
+        .subscribe(hoodMaster);**/
+
     this.angle.subscribe(new WpiSmartDashboard().getTextFieldDouble("angle_vis"));
     this.angle.map(x -> x < TOLERANCE_DEGREES).subscribe(new WpiSmartDashboard().getTextFieldBoolean("within tolerance, GO!"));
     this.distance.subscribe(new WpiSmartDashboard().getTextFieldDouble("distance_vis"));
