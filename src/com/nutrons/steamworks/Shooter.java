3170426b75dbc4691f057a4fb3626638615cbf88
nutronsds <nutronsds@gmail.com>
2017-03-31 14:10:23 -0400

    tuned shooter setpoints

--- src/com/nutrons/steamworks/Shooter.java
+++ src/com/nutrons/steamworks/Shooter.java
@@ -43,7 +43,7 @@ public class Shooter implements Subsystem {
     this.shooterButton = shooterButton;
     this.distance = distance;
     this.setpointHint = setpointHint;
-    this.variableSetpoint = this.distance.filter(x -> x != 0.0).map(x -> 9.76 * x + 1966.4).share();
+    this.variableSetpoint = this.distance.filter(x -> x != 0.0).map(x -> 11.05 * x + 1827).share();
   }
 
   public Command auto() {
