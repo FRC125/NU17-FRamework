d8e32fd4819236c0f1028b3d84e4bc891438799a
cappuccinocat <xinglydia@gmail.com>
2017-04-05 14:34:04 -0400

    added time limits to manual gear placer

--- src/com/nutrons/steamworks/FloorGearPlacer.java
+++ src/com/nutrons/steamworks/FloorGearPlacer.java
@@ -16,8 +16,8 @@ public class FloorGearPlacer implements Subsystem {
   private final static double CURRENT_THRESHOLD_WRIST = 4.0;
   // counterclockwise
   private final static double INTAKE_SPEED = -1.0; // collecting gear speed
-  private final static double WRIST_DESCENT_SPEED = -0.3;
-  private final static double WRIST_ASCENT_SPEED = 0.3;
+  private final static double ARM_DOWN_SPEED = 0.3;
+  private final static double ARM_UP_SPEED = -0.3;
   private final static double WRIST_PLACE_SPEED = -0.3; // descending again to place the gear onto the peg
   private final static double INTAKE_REVERSE_SPEED = 1.0; // expelling the gear onto the peg
   private final static double PLACE_TIMEOUT_TIME = 1.0; // seconds
@@ -25,8 +25,8 @@ public class FloorGearPlacer implements Subsystem {
   private final Flowable<Boolean> intakeButton;
   private final LoopSpeedController intakeMotor;
   private final LoopSpeedController wristMotor;
-  private final Flowable<Double> armUp;
-  private final Flowable<Double> armDown;
+  private final Flowable<Boolean> armUp;
+  private final Flowable<Boolean> armDown;
 
   public FloorGearPlacer(Flowable<Boolean> placeButton,
       Flowable<Boolean> intakeButton,
@@ -38,8 +38,8 @@ public class FloorGearPlacer implements Subsystem {
     this.intakeButton = intakeButton;
     this.intakeMotor = intakeMotor;
     this.wristMotor = wristMotor;
-    this.armUp = armUp;
-    this.armDown = armDown;
+    this.armUp = armUp.map(x -> x > 0.9 ? true : false).distinctUntilChanged();
+    this.armDown = armDown.map(x -> x > 0.9 ? true : false).distinctUntilChanged();
   }
 
   public Command pulse(){
@@ -53,13 +53,58 @@ public class FloorGearPlacer implements Subsystem {
     });
   }
 
+  public Command intake(){
+    return Command.just(x -> {
+      intakeMotor.runAtPower(INTAKE_SPEED);
+      return Flowable.just(() -> {
+        intakeMotor.runAtPower(0.0);
+      });
+    }).delayFinish(200, TimeUnit.MILLISECONDS).endsWhen(intakeButton.filter(y -> !y), true);
+  }
+
+  public Command place(){
+    return Command.just(x -> {
+      intakeMotor.runAtPower(INTAKE_REVERSE_SPEED);
+      return Flowable.just(() -> {
+        intakeMotor.runAtPower(0.0);
+      });
+    }).delayFinish(200, TimeUnit.MILLISECONDS).endsWhen(placeButton.filter(y -> !y), true);
+  }
+
+  public Command armUp(){
+    return Command.just(x -> {
+      wristMotor.runAtPower(ARM_UP_SPEED);
+      return Flowable.just(() -> {
+        wristMotor.runAtPower(0.0);
+      });
+    }).delayFinish(200, TimeUnit.MILLISECONDS).endsWhen(armUp.filter(y -> !y), true);
+  }
+
+  public Command armDown(){
+    return Command.just(x -> {
+      wristMotor.runAtPower(ARM_DOWN_SPEED);
+      return Flowable.just(() -> {
+        wristMotor.runAtPower(0.0);
+      });
+    }).delayFinish(200, TimeUnit.MILLISECONDS).endsWhen(armDown.filter(y -> !y), true);
+  }
+
   @Override
   public void registerSubscriptions() {
-    this.intakeButton.map(b -> b ? INTAKE_SPEED : 0.0).map(Events::power).subscribe(intakeMotor);
-    this.placeButton.map(b -> b ? INTAKE_REVERSE_SPEED : 0.0).map(Events::power).subscribe(intakeMotor);
+    //this.intakeButton.map(b -> b ? INTAKE_SPEED : 0.0).map(Events::power).subscribe(intakeMotor);
+    //this.placeButton.map(b -> b ? INTAKE_REVERSE_SPEED : 0.0).map(Events::power).subscribe(intakeMotor);
+    this.intakeButton.filter(x -> x).map(x -> intake().terminable(intakeButton.filter(y -> !y)))
+        .subscribe(x -> x.execute(true));
+    this.placeButton.filter(x -> x).map(x -> place().terminable(intakeButton.filter(y -> !y)))
+        .subscribe(x -> x.execute(true));
+    this.armUp.filter(x -> x).map(x -> armUp().terminable(armUp.filter(y -> !y)))
+        .subscribe(x -> x.execute(true));
+    this.armDown.filter(x -> x).map(x -> armDown().terminable(armDown.filter(y -> !y)))
+        .subscribe(x -> x.execute(true));
 
-    this.armUp.map(x -> x > 0.9 ? true : false).distinctUntilChanged().map(x -> x ? Events.power(-1.0) : Events.power(0.0)).subscribe(wristMotor);
-    this.armDown.map(x -> x > 0.9 ? true : false).distinctUntilChanged().map(x -> x ? Events.power(1.0) : Events.power(0.0)).subscribe(wristMotor);
+
+    //this.armUp.map(x -> x > 0.9 ? true : false).distinctUntilChanged().map(x -> x ? Events.power(-1.0) : Events.power(0.0)).subscribe(wristMotor);
+    //this.armDown.map(x -> x > 0.9 ? true : false).distinctUntilChanged().map(x -> x ? Events.power(1.0) : Events.power(0.0)).subscribe(wristMotor);
 
     FlowOperators.toFlow(() -> this.intakeMotor.getCurrent())
         .subscribe(new WpiSmartDashboard().getTextFieldDouble("intake current"));
