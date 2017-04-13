01cfb8e68efbd1ddaa5285a2f7999ef8cc3fd59e
cappuccinocat <xinglydia@gmail.com>
2017-04-13 19:31:39 -0400

    changed ports for ev

--- src/com/nutrons/steamworks/RobotMap.java
+++ src/com/nutrons/steamworks/RobotMap.java
@@ -1,31 +1,32 @@
 package com.nutrons.steamworks;
 
 public class RobotMap {
+  //Added three zeroes to everything
 
   // Intake or Shooter
-  public static final int SHOOTER_MOTOR_1 = 2;
-  public static final int SHOOTER_MOTOR_2 = 3;
+  public static final int SHOOTER_MOTOR_1 = 2000;
+  public static final int SHOOTER_MOTOR_2 = 3000;
   //TODO: Change climber ports to match robot.
-  public static final int CLIMBTAKE_MOTOR_1 = 30;
-  public static final int CLIMBTAKE_MOTOR_2 = 13;
+  public static final int CLIMBTAKE_MOTOR_1 = 30000;
+  public static final int CLIMBTAKE_MOTOR_2 = 13000;
   // TODO: Change hopper ports to match robot
-  public static final int TOP_HOPPER_MOTOR = 6;
-  public static final int SPIN_FEEDER_MOTOR = 4;
-  public static final int HOOD_MOTOR_A = 5;
+  public static final int TOP_HOPPER_MOTOR = 6000;
+  public static final int SPIN_FEEDER_MOTOR = 4000;
+  public static final int HOOD_MOTOR_A = 5000;
 
   // Ports of Drivetrain
-  public static final int FRONT_LEFT = 1;
-  public static final int BACK_LEFT = 20;
-  public static final int FRONT_RIGHT = 14;
-  public static final int BACK_RIGHT = 15;
+  public static final int FRONT_LEFT = 1000;
+  public static final int BACK_LEFT = 20000;
+  public static final int FRONT_RIGHT = 14000;
+  public static final int BACK_RIGHT = 15000;
 
   // Ports of Floor Gear Placer TODO: Fix Port to match Talons on placer
   public static final int WRIST_MOTOR = 19;
   public static final int INTAKE_MOTOR = 23;
 
   // Ports of Servos TODO: Fix Port to match robot servos
-  public static final int GEAR_SERVO_RIGHT = 7;
-  public static final int GEAR_SERVO_LEFT = 8;
+  public static final int GEAR_SERVO_RIGHT = 7000;
+  public static final int GEAR_SERVO_LEFT = 8000;
 
   // Controllers
   public static final int OP_PAD = 0;
